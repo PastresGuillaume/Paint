732c1f7c09822e11740da51d65862648cbf1144b
DtueurJ <102967168+DtueurJ@users.noreply.github.com>
2022-04-07 14:51:45 +0200

    Add files via upload

---
+++ src/graphics/shapes/ui/controllers/RectangleCreator.java
@@ -0,0 +1,167 @@
+package graphics.shapes.ui.controllers;
+
+import graphics.Constantes;
+import graphics.shapes.SCollection;
+import graphics.shapes.SRectangle;
+import graphics.shapes.Shape;
+import graphics.shapes.attributes.ColorAttributes;
+import graphics.shapes.attributes.SelectionAttributes;
+import graphics.shapes.ui.ShapesView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class RectangleCreator extends AbstractController {
+
+    private Shape model;
+    private ShapesView view;
+    private Point locCreation ;
+    private Color filledColor = new Color(0,0,0,1);
+    private Color strokedColor = Color.black;
+    final JPopupMenu menu = new JPopupMenu("Color Menu");
+
+    public RectangleCreator(Shape newModel, ShapesView view) {
+        super(newModel);
+        this.model = newModel;
+        this.view = view;
+        this.locCreation = new Point();
+        unselectedAll();
+
+        JMenu color = new JMenu("Color");
+        JMenuItem strokedColorMenu = new JMenuItem("Stroked color");
+        JMenuItem filledColorMenu = new JMenuItem("Filled color", new ImageIcon("graphics\\shapes\\ui\\image\\chat.jpg" ));
+
+        strokedColorMenu.addActionListener(e -> {
+            JFrame myFrame = new JFrame();
+            myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            myFrame.setSize(500, 300);
+            myFrame.setLocationRelativeTo(null);
+            myFrame.setLayout(new BorderLayout());
+            strokedColor = JColorChooser.showDialog(myFrame, "Pick a Color", Color.GREEN);
+            myFrame.setTitle("Select your filled color");
+        });
+        filledColorMenu.addActionListener(e -> {
+            JFrame myFrame = new JFrame();
+            myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            myFrame.setSize(500, 300);
+            myFrame.setLocationRelativeTo(null);
+            myFrame.setLayout(new BorderLayout());
+            filledColor = JColorChooser.showDialog(myFrame, "Pick a Color", Color.GREEN);
+            myFrame.setTitle("Select your filled color");
+        });
+
+        color.add(strokedColorMenu);
+        color.add(filledColorMenu);
+
+        menu.add(color);
+    }
+
+    public void setView(ShapesView view)
+    {
+        this.view = view;
+    }
+
+    public ShapesView getView()
+    {
+        return this.view;
+    }
+
+    public void setModel(Shape model)
+    {
+        this.model = model;
+    }
+
+    public Shape getModel() {return this.model;}
+
+    public void mousePressed(MouseEvent e) {
+        unselectedAll();
+        if(SwingUtilities.isRightMouseButton(e))
+        {
+            menu.show(view , e.getX(), e.getY());
+        }
+        else {
+            this.locCreation = e.getPoint();
+            SRectangle rect = new SRectangle(e.getPoint(), 10, 10);
+            rect.addAttributes(new ColorAttributes(true, true, filledColor, strokedColor));
+            rect.addAttributes(new SelectionAttributes());
+            ((SelectionAttributes) rect.getAttributes(Constantes.SELECTION_ATTRIBUTE)).select();
+            ((SCollection) this.model).add(rect);
+            view.invalidate();
+        }
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        this.locCreation = new Point();
+        unselectedAll();
+        view.invalidate();
+    }
+
+    public void mouseClicked(MouseEvent e)
+    {
+        this.locCreation = new Point();
+        unselectedAll();
+        view.invalidate();
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        {
+            int width = (int) (e.getPoint().getX() - this.locCreation.getX());
+            int height = (int) (e.getPoint().getY() - this.locCreation.getY());
+            this.changeSize(width, height);
+            System.out.println("Dragged");
+            view.invalidate();
+        }
+    }
+
+    public void keyPressed(KeyEvent evt)
+    {
+        int keyCode = evt.getKeyCode();
+        if(keyCode == KeyEvent.VK_SHIFT || keyCode == KeyEvent.VK_CONTROL) {
+            JFrame myFrame = new JFrame();
+            myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            myFrame.setSize(500, 300);
+            myFrame.setLocationRelativeTo(null);
+            myFrame.setLayout(new BorderLayout());
+            Color selectedColor = JColorChooser.showDialog(myFrame, "Pick a Color", Color.GREEN);
+            if(keyCode == KeyEvent.VK_SHIFT){
+                filledColor = selectedColor;
+                myFrame.setTitle("Select your filled color");
+            }
+            else {
+                strokedColor = selectedColor;
+                myFrame.setTitle("Select your stroked color");
+            }
+        }
+    }
+
+    public void keyReleased(KeyEvent evt)
+    {
+    }
+
+    private void unselectedAll(){
+        for (Shape s : ((SCollection) this.model).getElement())
+            s.unselect();
+    }
+
+    private void changeSize(int i,int p) {
+        for (Shape s : ((SCollection) this.model).getElement()) {
+            SelectionAttributes selectionAttributes = (SelectionAttributes) s.getAttributes(Constantes.SELECTION_ATTRIBUTE);
+            if (selectionAttributes != null && selectionAttributes.isSelected()) {
+                if (i < 0 && p < 0) {
+                    s.setLoc(new Point(this.locCreation.x + i,this.locCreation.y +p));
+                }
+                else if (i < 0) {
+                    s.setLoc((new Point(this.locCreation.x + i,this.locCreation.y)));
+                }
+                else if (p < 0) {
+                    s.setLoc((new Point(this.locCreation.x,this.locCreation.y + p)));
+                }
+                s.setSize(Math.abs(i), Math.abs(p));
+            }
+        }
+    }
+
+}
+
