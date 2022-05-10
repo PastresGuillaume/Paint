71da9d01e3762eb765bad9a8aceb5f199eea7599
Marion Yaghdjian <marion.yaghdjian@uha.fr>
2022-05-10 18:26:47 +0200

    NPNG javadoc de l'Audio

--- src/graphics/jeux/NoPaintNoGame/ecoute/Audio.java
+++ src/graphics/jeux/NoPaintNoGame/ecoute/Audio.java
@@ -7,54 +7,42 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
+/**
+ * Audio est la classe qui définit un son dans la javadoc
+ * Cette classe est caractérisée par les informations suivantes :
+ * <ul>
+ *  <li> Un Clip clip PAUSE</li>
+ * </ul>
+ *
+ * Audio est la classe qui permet d'inclure du son
+ */
 
 public class Audio {
 
-    //variable
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Création des donnees membres //
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /**
+     * Un Clip clip
+     */
 
     private Clip clip;
-    /*private boolean Sfond=true;
-    private boolean Sgagne=false;
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Constructeur //
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /**
+     * <b>Constructeur de Audio</b>
+     *
+     * @param son
+     *
+     */
 
 
-    public static boolean isSfond() {
-        return Sfond;
-    }
-
-    public static boolean isSgagne() {
-        return Sgagne;
-    }
-
-    public void setSfond(boolean sfond) {
-        Sfond = sfond;
-    }
-
-    public void setSgagne(boolean sgagne) {
-        Sgagne = sgagne;
-    }*/
-
-
-
-
-
-
-
-    //getter
-
-    public Clip getClip() {
-        return clip;
-        }
-
-    //Methodes
-    public void play(){clip.start();}
-
-    public void stop(){clip.stop();}
-
-
-     public Audio(String son){
+    public Audio(String son){
         try{
 //            AudioInputStream audio = AudioSystem.getAudioInputStream(getClass().getResource(son));
-            AudioInputStream audio = AudioSystem.getAudioInputStream(new File(Constantes.PATH_SON+"saut.wav"));
+            AudioInputStream audio = AudioSystem.getAudioInputStream(new File(Constantes.PATH_SON+"son.wav"));
             clip = AudioSystem.getClip();
             clip.open(audio);
         }
@@ -63,6 +51,27 @@ public class Audio {
         }
 
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Methodes//
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     *play lance le son
+     */
+    public void play(){clip.start();}
+
+    /**
+     *play arrete le son
+     */
+
+    public void stop(){clip.stop();}
+
+    /**
+     *playSound lance le son qui a pour nom "son"
+     *
+     * @param son
+     */
 
     public static void playSound(String son){
         Audio s = new Audio(son);
